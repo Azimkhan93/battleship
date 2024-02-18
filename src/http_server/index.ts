b8f5fbc29e2b68d4a957325044c784a92c9bf27d
Azimkhan93 <aabdulsatarov@gmail.com>
2024-02-19 03:21:59 +0500

    feat: add websocket and login

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -1,6 +1,8 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as http from 'http';
+import Websocket, { WebSocketServer } from 'ws';
+import crypto from 'crypto';
 
 export const httpServer = http.createServer(function (req, res) {
     const __dirname = path.resolve(path.dirname(''));
@@ -14,4 +16,17 @@ export const httpServer = http.createServer(function (req, res) {
         res.writeHead(200);
         res.end(data);
     });
+});
+
+const wsServer = new WebSocketServer({ port: 3000});
+
+wsServer.on('connection', (ws) => {
+    const userID = crypto.randomUUID();
+    console.log('New client connected', userID);
+    ws.on('message', (message) => {
+        const stringifiedMessage = message.toString('utf-8')        
+        const parsedJSON = JSON.parse(stringifiedMessage)
+        console.log(parsedJSON.data);
+        ws.send(stringifiedMessage)
+    })
 });
